8531c4c530c630cc9eb4e8a6d14d6c0e9b13fe88
Matheus Wander <wanderzin98@gmail.com>
2024-01-15 12:27:55 -0300

    glitch in the simulation

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,10 +1,40 @@
+import 'package:expenses_app/models/expense.dart';
 import 'package:flutter/material.dart';
 
-class ExpensesMainScreen extends StatelessWidget {
-  const ExpensesMainScreen({super.key});
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Flutter Course',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'Cinema',
+        amount: 50.99,
+        date: DateTime.now(),
+        category: Category.leisure),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Ni háo ninja'));
+    return const Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'The chart',
+            style: TextStyle(
+                color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
   }
 }
