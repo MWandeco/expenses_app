bb551c6fc0ccc9932a753aa8c1d47e92cc866464
Matheus Wander <wanderzin98@gmail.com>
2024-01-15 11:38:46 -0300

    Initial commit

---
+++ lib/expenses.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ExpensesMainScreen extends StatelessWidget {
+  const ExpensesMainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Ni háo ninja'));
+  }
+}
