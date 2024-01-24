5751383d336ba9caabb96ede7b386260fc7e4e4a
Matheus Wander <wanderzin98@gmail.com>
2024-01-24 11:25:53 -0300

    Added ChartBars
    Added chartbars for users to see where they spend most of their money, and some minor ui/ux improvements

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:expenses_app/models/expense.dart';
+import 'package:expenses_app/widgets/chart/chart.dart';
 import 'package:expenses_app/widgets/expenses_list/expenses_list.dart';
 import 'package:expenses_app/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
@@ -83,11 +84,7 @@ class _ExpensesState extends State<Expenses> {
       ]),
       body: Column(
         children: [
-          const Text(
-            'The chart',
-            style: TextStyle(
-                color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
-          ),
+          Chart(expenses: _registeredExpenses),
           Expanded(child: mainContent),
         ],
       ),
